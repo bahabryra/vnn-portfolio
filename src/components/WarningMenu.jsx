28024d38b92686d3364c1aeb4135f27a08ccc296
bahabryra <160906216+bahabryra@users.noreply.github.com>
2026-01-15 10:06:16 -0600

    warning

--- src/components/WarningMenu.jsx
+++ src/components/WarningMenu.jsx
@@ -38,9 +38,7 @@ const WarningMenu = () => {
                         <IoWarningSharp />
                     </div>
                     <div className="font-rodin font-black text-3xl md:text-5xl text-white md:ml-4">
-                        WARNING- YOU ARE ABOUT TO VIEW MY PORTFOLIO 
-                        <br>ITS SO GOOD YOU MAY GET PARALYZED FOR THE REST OF YOUR LIFE 
-                        <br> YOUVE BEEN WARNED</br>
+                        WARNING- YOU ARE ABOUT TO VIEW MY PORTFOLIO ITS SO GOOD YOU MAY GET PARALYZED FOR THE REST OF YOUR LIFE YOUVE BEEN WARNED
                     </div>
                 </div>
                 <div className="flex items-center mt-24">
